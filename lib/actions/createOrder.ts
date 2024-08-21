65f0c0f02183fa422750aba21fee0addf6b21523
Marcin Fraś <marcinfras2003@gmail.com>
2024-08-21 20:04:01 +0200

    Add stripe

--- lib/actions/createOrder.ts
+++ lib/actions/createOrder.ts
@@ -1,5 +1,21 @@
 "use server";
 
+import { getEnv } from "@/app/utils/utils";
+import { Stripe } from "stripe";
+import { fetchCart } from "./fetchCart";
+
+const stripeClient = new Stripe(getEnv(process.env.STRIPE_KEY), {
+  apiVersion: "2024-06-20",
+});
+
 export const createOrder = async () => {
-  console.log("hiiiiiiiiiiiiiiiiiiii");
+  const cart = await fetchCart();
+
+  const stripeBill = await stripeClient.paymentLinks.create({
+    payment_method_types: ["p24"],
+    line_items: [{ price: "100", quantity: 1 }],
+    currency: "USD",
+  });
+  console.log("Billllllllllllll: " + stripeBill);
+  return stripeBill;
 };
