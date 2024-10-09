e7c0bd701f8bffad6258bdd65023e7269d009685
Marcin Fraś <marcinfras2003@gmail.com>
2024-10-09 20:03:27 +0200

    feat: delete named export from LoginLayout

--- src/app/(login-register)/layout.tsx
+++ src/app/(login-register)/layout.tsx
@@ -1,9 +1,8 @@
-
 import { LoginRegisterFormTemplate } from "./_components/LoginRegisterFormTemplate";
 
 //https://v0.dev/r/gl6VD99Wth9
 
-export const LoginLayout = ({ children }: { children: React.ReactNode }) => {
+const LoginLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 dark:bg-gray-950 px-4 py-12">
       <LoginRegisterFormTemplate>{children}</LoginRegisterFormTemplate>
