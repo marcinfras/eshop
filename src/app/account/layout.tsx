4c19ddfd8ec083a99dc2674a1228b3cee79c0f86
Marcin Fraś <marcinfras2003@gmail.com>
2024-10-09 20:09:09 +0200

    feat: delete named export in AccountLayout

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,6 +1,6 @@
 import { AccountPageHeader } from "./_components/AccountPageHeader";
 
-export const AccountLayout = ({ children }: { children: React.ReactNode }) => {
+const AccountLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <AccountPageHeader />
