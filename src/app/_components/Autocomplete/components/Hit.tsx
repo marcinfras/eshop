6043eca5816597459781dc77a1108f58c3910fea
Marcin Fraś <marcinfras2003@gmail.com>
2024-11-20 21:24:52 +0100

    fix: fix product image is out the box in search modal

--- src/app/_components/Autocomplete/components/Hit.tsx
+++ src/app/_components/Autocomplete/components/Hit.tsx
@@ -18,14 +18,16 @@ export const Hit = ({ hit }: HitProps) => {
   return (
     <Link href={`/products/${hit.slug}`} className="cursor-pointer">
       <article className="w-full h-full p-4 flex items-center gap-3">
-        <Image
-          alt={hit.name}
-          className="w-[100px] h-auto"
-          src={hit.images[0].url}
-          width={800}
-          height={800}
-        />
-        <div className="flex flex-col gap-1">
+        <div className="w-[70px]  sm:w-[100px]">
+          <Image
+            alt={hit.name}
+            className="max-h-[100px] w-auto m-auto"
+            src={hit.images[0].url}
+            width={800}
+            height={800}
+          />
+        </div>
+        <div className="flex flex-col gap-1 w-full">
           <div className="hit-name font-medium">
             <Highlight attribute="name" hit={hit} />
           </div>
